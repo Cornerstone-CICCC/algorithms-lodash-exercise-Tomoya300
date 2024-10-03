3a8378bfce2bbb66146e362c28d65a8dabf67e76
Tomoya <flyingfox193807@gmail.com>
2024-10-03 11:06:08 -0700

    finished ex01.js

--- ex01.js
+++ ex01.js
@@ -4,6 +4,10 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5];
 
-// your code here
+// function multiple(n) {
+//     return n*2
+// }
+
+const doubled = _.map(numbers, (item) => item*2)
 
 console.log(doubled); // Expected output: [2, 4, 6, 8, 10]
